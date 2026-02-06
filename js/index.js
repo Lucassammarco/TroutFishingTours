06cb3b7e1d17ec3d480326e3fd80139f89924f60
Lucas Sammarco <lucas_sammarco@hotmail.com>
2026-02-06 20:52:07 -0300

    dale funciona

---
+++ js/index.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const slides = document.querySelectorAll(".slide");
+    let current = 0;
+
+    function showSlide(index) {
+        slides.forEach((slide, i) => {
+            slide.classList.remove("active");
+            if (i === index) {
+                slide.classList.add("active");
+            }
+        });
+    }
+
+    function nextSlide() {
+        current = (current + 1) % slides.length;
+        showSlide(current);
+    }
+
+    setInterval(nextSlide, 7000); // cambia cada 5 segundos
+});
